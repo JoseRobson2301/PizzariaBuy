3fafd9f5175fd2ea8294dfe66f5b2a6c2a0d2b44
Jose Robson <111148505+JoseRobson2301@users.noreply.github.com>
2023-05-30 17:22:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+function toggleDisplay(element) {
+    var style = window.getComputedStyle(element);
+    if (style.display === 'none') {
+        element.style.display = 'block';
+    } else {
+        element.style.display = 'none';
+    }
+}
+
+var cardapioBtn = document.getElementById('cardapioBtn');
+var navbar = document.querySelector('.navbar');
+
+cardapioBtn.addEventListener('click', function() {
+    toggleDisplay(navbar);
+});
+
+var clickPizzaBtn = document.getElementById('clickPizzaBtn');
+var opctionFood = document.querySelector('.opctionFood');
+
+clickPizzaBtn.addEventListener('click', function() {
+    toggleDisplay(opctionFood);
+});
+
+var clickPastelBtn = document.getElementById('clickPastelBtn');
+var opctionFood02 = document.querySelector('.opctionFood02');
+
+clickPastelBtn.addEventListener('click', function() {
+    toggleDisplay(opctionFood02);
+});
+
+var clickCoxinhaBtn = document.getElementById('clickCoxinhaBtn');
+var opctionFood03 = document.querySelector('.opctionFood03');
+
+clickCoxinhaBtn.addEventListener('click', function() {
+    toggleDisplay(opctionFood03);
+});
+
+var clickEmpadaBtn = document.getElementById('clickEmpadaBtn');
+var opctionFood04 = document.querySelector('.opctionFood04');
+
+clickEmpadaBtn.addEventListener('click', function() {
+    toggleDisplay(opctionFood04);
+});
+
+function fecharAnuncio() {
+    var anuncio = document.getElementById("menu");
+    anuncio.style.display = "none";
+};
+
+function buyStore() {
+    window(Swal.fire({
+        title: 'Deseja agora?',
+        text: "Você deseja comprar agora!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Comfirmar a compra'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Sucesso!',
+            'Sua compra foi um sucesso.',
+            'success'
+          )
+        }
+      }))
+}
